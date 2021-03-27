effa85f89610a057126186e165b1e06887aea9fe
Vladimir Varbanov <vladimir.varbanov@puzzel.com>
2021-03-27 12:45:45 +0200

    Add: Cat single page entry VIA paramenters

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React, { Fragment } from "react";
 import { Router } from "@reach/router";
 /** importing our pages */
 import Cats from "./cats";
+import SpaceCat from "./space-cat";
 
 export default function Pages() {
   return (
     <Router primary={false} component={Fragment}>
       <Cats path="/" />
+      <SpaceCat catName={"the-lazy-gonzo"} path="/cat" />
     </Router>
   );
 }
