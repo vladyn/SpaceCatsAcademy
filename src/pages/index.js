d1dd4e2f6f652d84b60f810835fc72959454fb66
Vladimir Varbanov <vladimir.varbanov@puzzel.com>
2021-03-28 17:35:50 +0300

    Add: A component and page to display static pages. Fix: Re-factoring

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import { Router } from "@reach/router";
 /** importing our pages */
 import Cats from "./cats";
 import SpaceCat from "./space-cat";
+import About from "./about";
 
 export default function Pages() {
   return (
     <Router primary={false} component={Fragment}>
-      <Cats path="/" />
-      <SpaceCat catName={"the-lazy-gonzo"} path="/cat" />
+      <About path="/about" />
+      <Cats path="/cats" />
+      <SpaceCat path="/cat/:name" />
     </Router>
   );
 }
