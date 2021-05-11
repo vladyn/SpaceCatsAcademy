ba37f02513d53efb601aa756baf0d3c843147ce2
Vladimir Varbanov <vladimir.varbanov@puzzel.com>
2021-05-11 15:46:39 +0300

    Add more space to the container on narrower screens. Add: Spacings

--- src/components/split-section.js
+++ src/components/split-section.js
@@ -1,15 +1,16 @@
-import React from "react";
-import styled from "@emotion/styled";
+import React from 'react'
+import styled from '@emotion/styled'
+import { widths, mq } from '../styles'
 
 // import { widths, colors } from "../styles";
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types'
 
 /**
  * Two column layout on desktop
  */
 
 const SplitSection = ({ children, iconLeft, iconRight }) => {
-  const spaceOutVertical = iconLeft || iconRight ? "105px" : "0px";
+  const spaceOutVertical = iconLeft || iconRight ? '105px' : '0px'
 
   return (
     <ContentDiv
@@ -19,38 +20,49 @@ const SplitSection = ({ children, iconLeft, iconRight }) => {
     >
       {children}
     </ContentDiv>
-  );
-};
+  )
+}
 
-export default SplitSection;
+export default SplitSection
 
 const ContentDiv = styled.div(({ iconRight, iconLeft, marginTop }) => ({
-  display: "flex",
-  flexDirection: "row",
-  flexWrap: "wrap",
-  width: "100%",
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  width: '100%',
+  alignItems: 'center',
+  alignSelf: 'center',
+  [mq[0]]: {
+    width: '90%'
+  },
+  [mq[1]]: {
+    width: '55%'
+  },
+  [mq[2]]: {
+    width: '90%'
+  },
   div: {
     paddingTop: marginTop,
-    display: "flex",
-    flexDirection: "column",
-    flexBasis: "100%",
-    flex: "1",
-    ":first-child": {
+    display: 'flex',
+    flexDirection: 'column',
+    flexBasis: '100%',
+    flex: '1',
+    ':first-child': {
       backgroundImage: `url(${iconLeft})`,
-      backgroundPosition: "top left",
-      backgroundRepeat: "no-repeat",
-      paddingRight: "1em",
+      backgroundPosition: 'top left',
+      backgroundRepeat: 'no-repeat',
+      paddingRight: '1em'
     },
-    ":last-child": {
+    ':last-child': {
       backgroundImage: `url(${iconRight})`,
-      backgroundPosition: "top left",
-      backgroundRepeat: "no-repeat",
-      paddingLeft: "1em",
-    },
-  },
-}));
+      backgroundPosition: 'top left',
+      backgroundRepeat: 'no-repeat',
+      paddingLeft: '1em'
+    }
+  }
+}))
 
 SplitSection.propTypes = {
   iconLeft: PropTypes.string,
-  iconRight: PropTypes.string,
-};
+  iconRight: PropTypes.string
+}
