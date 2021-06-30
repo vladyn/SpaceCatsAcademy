3d4648afdea6d05ea6f3406a1c37dc229a3408cb
Vladimir Varbanov <vladimir.varbanov@puzzel.com>
2021-06-30 11:59:01 +0300

    Responsive Responsive Add: responsive navigation

--- src/components/main-navigation.js
+++ src/components/main-navigation.js
@@ -3,6 +3,8 @@ import styled from "@emotion/styled";
 import { Link } from "@reach/router";
 import { colors } from "../styles";
 import { mq } from "../styles";
+import { IconClose } from "@apollo/space-kit/icons/IconClose";
+import { IconMenu } from "@apollo/space-kit/icons/IconMenu";
 
 /**
  * Main navigation.
@@ -11,10 +13,16 @@ import { mq } from "../styles";
 
 const MainNavigation = () => {
   return (
-    <NavWrapper>
+    <NavWrapper id="menu">
       <NavList>
         <li>
           <Link to="/">Home</Link>
+          <Link to="#" className="menu-responsive" id="menu_close">
+            <IconClose href="#" id="menu_close"></IconClose>
+          </Link>
+          <Link to="#menu" className="menu-responsive" id="menu_open">
+            <IconMenu href="#menu"></IconMenu>
+          </Link>
         </li>
         <li>
           <Link to="/about">About</Link>
@@ -40,16 +48,36 @@ export default MainNavigation;
 
 /** Main navigation styles components */
 const NavWrapper = styled.nav({
-  flex: 1,
+  [mq[1]]: {
+    flex: 1,
+    backgroundColor: "transparent",
+    width: "300px",
+    height: "200px",
+    display: "block",
+  },
+  display: "none",
+  ":target": {
+    display: "block",
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    width: "97.5vw",
+    height: "92vh",
+  },
 });
 
 const NavList = styled.ul({
   listStyle: "none",
   display: "block",
   "& li": {
-    display: "block",
+    border: "1px solid navy",
+    display: "flex",
+    justifyContent: "space-between",
     "& a": {
       fontSize: "2em",
+    },
+
+    // on mobile show the hamburger menu icon
+    "& a#menu_open": {
+      display: "block",
     },
   },
   flexDirection: "row",
@@ -62,16 +90,17 @@ const NavList = styled.ul({
     color: colors.mainNavPrimary,
     display: "block",
     padding: "1px 12px 32px 12px",
+    border: "1px solid red",
   },
   "a:hover": {
     color: "#ABAC6C",
   },
   'a[aria-current="page"]': {
-    fontWeight: "bold",
-    fontFamily: "'Lato bold', sans-serif",
-    borderBottom: `2px solid ${colors.mainNavHoverBorderColor}`,
+    color: colors.mainNavHoverBorderColor,
+    borderBottom: "none",
   },
-  // Mobile
+
+  // Desktop
   [mq[1]]: {
     display: "flex",
     "& li": {
@@ -79,6 +108,14 @@ const NavList = styled.ul({
       "& a": {
         fontSize: 11,
       },
+      "& .menu-responsive": {
+        display: "none",
+      },
+    },
+    'a[aria-current="page"]': {
+      fontWeight: "bold",
+      fontFamily: "'Lato bold', sans-serif",
+      borderBottom: `2px solid ${colors.mainNavHoverBorderColor}`,
     },
   },
 });
